efc31283f7d5471248f0b0d11f5ecdf38d39beca
Suyash Baoney <suyashbaoney58@gmail.com>
2026-08-31 00:06:11 +0530

    fix: lifespan error in fastapi

--- fastapi_backend/app/main.py
+++ fastapi_backend/app/main.py
@@ -49,7 +49,7 @@ logger = logging.getLogger(__name__)
 # --- FastAPI App Initialization ---
 
 app = FastAPI(
-    lifespan=lifespan,
+    # lifespan=lifespan,
     title="FastAPI & SQLAlchemy Async Backend"
 )
 
